c9ac5a67d7b65f52b5add767ab44d2cdaf9dadd8
Unknown <Shamacron@gmail.com>
2017-10-09 11:00:02 -0600

    Updated comments
    Updated the comments for the methods in the Board class to be java doc comments

--- src/main/java/edu/neumont/csc130/EMB/Board.java
+++ src/main/java/edu/neumont/csc130/EMB/Board.java
@@ -5,25 +5,28 @@ import java.util.Map;
 
 public class Board
 {
+    /**
+     * heaps is the hashmap that acts as the game board
+     */
     private Map<String, Integer> heaps = new HashMap<String, Integer>();
 
     //Creates the board based on difficulty
     public Board(int difficulty)
     {
-        if(difficulty == 1)
+        if (difficulty == 1)
         {
             //If the selected difficulty is 1 (easy)
             //Create 2 heaps with 2 and 2 tokens respectively
             heaps.put("A", 2);
             heaps.put("B", 2);
-        }else if(difficulty == 2)
+        } else if (difficulty == 2)
         {
             //If the selected difficulty is 2 (medium)
             //Create 3 heaps with 2, 5, and 7 tokens respectively
             heaps.put("A", 2);
             heaps.put("B", 5);
             heaps.put("C", 7);
-        }else
+        } else
         {
             //If the selected difficulty is 3 (hard)
             //Create 4 heaps with 2, 3, 8, and 9 tokens respectively
@@ -34,7 +37,11 @@ public class Board
         }
     }
 
-    //isValidHeapChoice checks to make sure that the chosen heap is valid
+    /**
+     * isValidHeapChoice checks to make sure that the chosen heap is valid
+     * @param heap
+     * @return boolean
+     */
     public boolean isValidHeapChoice(String heap)
     {
         if (!heaps.containsKey(heap) || heaps.get(heap) <= 0)
@@ -48,7 +55,12 @@ public class Board
         return true;
     }
 
-    //isValidTokenChoice checks to make sure that the amount of tokens to be removed is valid
+    /**
+     * isValidTokenChoice checks to make sure that the amount of tokens to be removed is valid
+     * @param tokensToRemove
+     * @param chosenHeap
+     * @return boolean
+     */
     public boolean isValidTokenChoice(int tokensToRemove, String chosenHeap)
     {
         //Added String chosenHeap to the parameters
@@ -71,7 +83,11 @@ public class Board
         return true;
     }
 
-    //takes in the chosen heap and amount of tokens to be removed and removes them from the hashmap
+    /**
+     * takes in the chosen heap and amount of tokens to be removed and removes them from the hashmap
+     * @param heap
+     * @param tokensToRemove
+     */
     public void removeTokens(String heap, int tokensToRemove)
     {
         //takes in the chosen heap and changes the amount of tokens in the heap
@@ -79,7 +95,10 @@ public class Board
         heaps.replace(heap, getTokensInHeap(heap) - tokensToRemove);
     }
 
-    //checks to see if the game is in a winning (losing?) state
+    /**
+     * checks to see if the game is in a winning (losing?) state
+     * @return boolean
+     */
     public boolean checkForLoss()
     {
         for (Integer tokens : heaps.values())
@@ -96,7 +115,11 @@ public class Board
         return true;
     }
 
-    //gets the amount of tokens in the specified heap
+    /**
+     * gets the amount of tokens in the specified heap
+     * @param key
+     * @return
+     */
     public int getTokensInHeap(String key)
     {
         return heaps.get(key);
