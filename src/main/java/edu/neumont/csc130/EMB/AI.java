0ea91bbf4a49d28319bd10f8e76a6fc41bde972e
Emily Parr <eparr@student.neumont.edu>
2017-10-09 21:46:19 -0600

    Finished AI class

--- src/main/java/edu/neumont/csc130/EMB/AI.java
+++ src/main/java/edu/neumont/csc130/EMB/AI.java
@@ -1,9 +1,37 @@
 package edu.neumont.csc130.EMB;
 
-public class AI extends Player{
-	
+import java.util.Random;
+
+public class AI extends Player {
+
 	public AI() {
-		super(); 
+	}
+
+	@Override
+	public void takeTurn(Board board) {
+		Random rand = new Random();
+		
+		String heapChoice = null;
+		boolean valid = false;
+		do {
+			heapChoice = Character.toString((char)(rand.nextInt(4)+65));
+			System.out.println(heapChoice);
+			heapChoice = heapChoice.toUpperCase();
+			if (board.isValidHeapChoice(heapChoice)) {
+				valid = true;
+			}
+		} while (!valid);
+		valid = false; 
+
+		int tokenChoice;
+		do {
+			tokenChoice = rand.nextInt(9)+1; 
+			if (board.isValidTokenChoice(tokenChoice, heapChoice)) {
+				valid = true;
+			}
+		} while (!valid);
+		
+		board.removeTokens(heapChoice, tokenChoice);
 	}
 
 }
