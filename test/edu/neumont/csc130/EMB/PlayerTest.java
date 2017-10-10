450cda3c9a936cd24a42f50e63488037cdd288b9
Emily Parr <eparr@student.neumont.edu>
2017-10-10 00:13:23 -0600

    Tests for Player class Added
    Don't know how to test for takeTurn

--- test/edu/neumont/csc130/EMB/PlayerTest.java
+++ test/edu/neumont/csc130/EMB/PlayerTest.java
@@ -7,14 +7,31 @@ import static org.junit.Assert.*;
 public class PlayerTest {
   @Test
   public void getName() throws Exception {
+	  Player p = new Player();
+	  p.setName("Bill");
+	  assertTrue(p.getName().equals("Bill"));
+  }
+  
+  @Test
+  public void isValidName() {
+	  Player p = new Player();
+	  assertTrue(p.isValidName("harry")); 
+	  assertTrue(p.isValidName(" Rhonda"));
+	  assertTrue(p.isValidName("888"));
+	  assertFalse(p.isValidName("jo%n"));
+	  assertFalse(p.isValidName("sd 8"));
   }
 
   @Test
   public void setName() throws Exception {
+	  Player p = new Player(); 
+	  p.setName("Ron");
+	  assertTrue(p.getName().equals("Ron"));
   }
 
   @Test
   public void takeTurn() throws Exception {
+	  // I don't know how to test for this besides running it? 
   }
 
 }
