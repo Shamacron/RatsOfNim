76dad9aa0837f8a6640bcc93966d8e28b44661af
Emily Parr <eparr@student.neumont.edu>
2017-10-09 22:57:57 -0600

    Commentation added to Player and AI Class

--- src/main/java/edu/neumont/csc130/EMB/AI.java
+++ src/main/java/edu/neumont/csc130/EMB/AI.java
@@ -4,34 +4,49 @@ import java.util.Random;
 
 public class AI extends Player {
 
+	/**
+	 * creates AI object
+	 */
 	public AI() {
 		setName("Computer");
 	}
 
+	/**
+	 * randomly chooses heap and token amounts, validates, and removes tokens
+	 */
 	@Override
 	public void takeTurn(Board board) {
 		Random rand = new Random();
 		
+		// heap randomization
 		String heapChoice = null;
 		boolean valid = false;
 		do {
+			// random integer conversion to Char, and then to String
 			heapChoice = Character.toString((char)(rand.nextInt(4)+65));
-			System.out.println(heapChoice);
-			heapChoice = heapChoice.toUpperCase();
+			// validates random choice validity
+			// returns true if possible, false if not
+			// loops until valid heap
 			if (board.isValidHeapChoice(heapChoice)) {
 				valid = true;
 			}
 		} while (!valid);
 		valid = false; 
 
+		// token randomization
 		int tokenChoice;
 		do {
+			// generates random int for tokens to remove
 			tokenChoice = rand.nextInt(9)+1; 
+			// validates random choice validity
+			// returns true if possible, false if not
+			// loops until valid token choice
 			if (board.isValidTokenChoice(tokenChoice, heapChoice)) {
 				valid = true;
 			}
 		} while (!valid);
 		
+		// removes tokens
 		board.removeTokens(heapChoice, tokenChoice);
 	}
 
--- src/main/java/edu/neumont/csc130/EMB/Player.java
+++ src/main/java/edu/neumont/csc130/EMB/Player.java
@@ -1,40 +1,67 @@
 package edu.neumont.csc130.EMB;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.regex.Pattern;
 
 public class Player {
 
+	/**
+	 * name is the player's name
+	 */
 	private String name;
 
+	/**
+	 * creates a person object
+	 */
 	public Player() {
 	}
 
+	/**
+	 * returns player's name
+	 * @return String
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * sets the player's name
+	 * @param name
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * validates the player's input for name
+	 * @param name
+	 * @return boolean
+	 */
 	public boolean isValidName(String name) {
+		// only accepts names that are alphanumeric, excluding symbols and whitespace
+		// returns false if name is not valid
 		String regex = "^[a-zA-Z0-9]+$";
 		return Pattern.compile(regex, Pattern.MULTILINE).matcher(name).matches();
 	}
 
 
-
+	/**
+	 * requests the player's input for heap and heap choice, validates
+	 * the board removes the tokens chosen by player
+	 * @param board
+	 */
 	public void takeTurn(Board board) {
+		// heap choice
 		System.out.println("Enter the corresponding letter of the heap you would like to remove token(s) from:");
 
 		String heapChoice = null;
 		boolean valid = false;
 		do {
+			// accepts input through the game class 
 			heapChoice = Nim.getConsoleInput();
 			heapChoice = heapChoice.toUpperCase();
+			// validates the heap choice through the board class
+			// returns true if the heap contains tokens, false if empty
+			// if false, requests user input again
 			if (board.isValidHeapChoice(heapChoice)) {
 				valid = true;
 			} else {
@@ -43,12 +70,16 @@ public class Player {
 		} while (!valid);
 		valid = false; 
 
-		
+		// token choice
 		System.out.println("Enter the number of tokens you would like to remove from heap " + heapChoice + ":");
 
 		int tokenChoice;
 		do {
+			// accepts user input through the game class
 			tokenChoice = Integer.parseInt(Nim.getConsoleInput());
+			// validates if the amount of tokens in chosen heap can be chosen
+			// returns true if possible user choice, false if not
+			// if flase, requests user input again
 			if (board.isValidTokenChoice(tokenChoice, heapChoice)) {
 				valid = true;
 			} else {
@@ -56,6 +87,7 @@ public class Player {
 			}
 		} while (!valid);
 		
+		// removes tokens through board class
 		board.removeTokens(heapChoice, tokenChoice);
 
 	}
